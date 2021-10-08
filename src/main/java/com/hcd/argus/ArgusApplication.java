0e539597190a9852fea2cb2aa5d4abb940d2e474
horatiucd <horatiucd@gmai.com>
2021-10-08 16:25:40 +0300

    Initial VDR Project

---
+++ src/main/java/com/hcd/argus/ArgusApplication.java
@@ -0,0 +1,13 @@
+package com.hcd.argus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArgusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hcd/argus/ArgusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcd.argus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
